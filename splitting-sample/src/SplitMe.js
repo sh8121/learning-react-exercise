27f7a7158ddf2d686274c4866a0c1a866c97e8f8
sh8121 <yozzangga@gmail.com>
2019-12-30 17:42:34 +0900

    Commit before eject

---
+++ splitting-sample/src/SplitMe.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class SplitMe extends React.Component {
+	render() {
+		return <div>SplitMe</div>;
+	}
+}
+
+// const SplitMe = () => {
+//   return <div>SplitMe</div>;
+// };
+
+export default SplitMe;
