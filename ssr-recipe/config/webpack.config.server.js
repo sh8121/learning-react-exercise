0bc7b7167b6292ec95c9cbb691130591a6417b36
sh8121 <yozzangga@gmail.com>
2020-01-04 09:56:39 +0900

    ssr-recipe

---
+++ ssr-recipe/config/webpack.config.server.js
@@ -0,0 +1,122 @@
+const paths = require("./paths");
+const getCSSModuleLocalIdent = require("react-dev-utils/getCSSModuleLocalIdent");
+const nodeExternals = require("webpack-node-externals");
+const cssRegex = /\.css$/;
+const cssModuleRegex = /\.module\.css$/;
+const sassRegex = /\.(sass|scss)$/;
+const sassModuleRegex = /\.module\.(sass|scss)$/;
+const webpack = require("webpack");
+const getClientEnvironment = require("./env");
+const publicUrl = paths.servedPath.slice(0, -1);
+const env = getClientEnvironment(publicUrl);
+
+module.exports = {
+	mode: "production",
+	entry: paths.ssrIndexJs,
+	target: "node",
+	output: {
+		path: paths.ssrBuild,
+		filename: "server.js",
+		chunkFilename: "js/[name].chunk.js",
+		publicPath: paths.servedPath
+	},
+	module: {
+		rules: [
+			{
+				oneOf: [
+					{
+						test: /\.(js|mjs|jsx|ts|tsx)$/,
+						include: paths.appSrc,
+						loader: require.resolve("babel-loader"),
+						options: {
+							customize: require.resolve(
+								"babel-preset-react-app/webpack-overrides"
+							),
+							plugins: [
+								[
+									require.resolve("babel-plugin-named-asset-import"),
+									{
+										loaderMap: {
+											svg: {
+												ReactComponent: "@svgr/webpack?-svgo![path]"
+											}
+										}
+									}
+								]
+							],
+							cacheDirectory: true,
+							cacheCompression: false,
+							compact: false
+						}
+					},
+					{
+						test: cssRegex,
+						exclude: cssModuleRegex,
+						loader: require.resolve("css-loader"),
+						options: {
+							onlyLocals: true
+						}
+					},
+					{
+						test: cssModuleRegex,
+						loader: require.resolve("css-loader"),
+						options: {
+							modules: true,
+							onlyLocals: true,
+							getLocalIdent: getCSSModuleLocalIdent
+						}
+					},
+					{
+						test: sassRegex,
+						exclude: sassModuleRegex,
+						use: [
+							{
+								loader: require.resolve("css-loader"),
+								options: {
+									onlyLocals: true
+								}
+							},
+							require.resolve("sass-loader")
+						]
+					},
+					{
+						test: sassModuleRegex,
+						use: [
+							{
+								loader: require.resolve("css-loader"),
+								options: {
+									modules: true,
+									onlyLocals: true,
+									getLocalIdent: getCSSModuleLocalIdent
+								}
+							},
+							require.resolve("sass-loader")
+						]
+					},
+					{
+						test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+						loader: require.resolve("url-loader"),
+						options: {
+							emitFile: false,
+							limit: 10000,
+							name: "static/media/[name].[hash:8].[ext]"
+						}
+					},
+					{
+						loader: require.resolve("file-loader"),
+						exclude: [/\.(js|mjs|jsx|ts|tsx)$/, /\.html$/, /\.json$/],
+						options: {
+							emitFile: false,
+							name: "static/media/[name].[hash:8].[ext]"
+						}
+					}
+				]
+			}
+		]
+	},
+	resolve: {
+		modules: ["node_modules"]
+	},
+	externals: [nodeExternals()],
+	plugins: [new webpack.DefinePlugin(env.stringified)]
+};
